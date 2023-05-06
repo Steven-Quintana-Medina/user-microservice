dc2f0362e62da6e0e9f7a0f478339b4539c3ef1b
steven quintana <quintanasteven57@gmail.com>
2023-05-05 22:41:56 -0500

    change: The valid dni and insertion of roles was added✨

--- src/main/java/com/pragma/powerup/usermicroservice/domain/usecase/UserUseCase.java
+++ src/main/java/com/pragma/powerup/usermicroservice/domain/usecase/UserUseCase.java
@@ -3,6 +3,7 @@ package com.pragma.powerup.usermicroservice.domain.usecase;
 import com.pragma.powerup.usermicroservice.domain.api.IUserServicePort;
 import com.pragma.powerup.usermicroservice.domain.model.User;
 import com.pragma.powerup.usermicroservice.domain.services.UserService;
+import com.pragma.powerup.usermicroservice.domain.spi.IRolPersistencePort;
 import com.pragma.powerup.usermicroservice.domain.spi.IUserPersistencePort;
 
 public class UserUseCase implements IUserServicePort {
@@ -15,13 +16,16 @@ public class UserUseCase implements IUserServicePort {
     @Override
     public void saveClient(User user) {
         UserService.ValidPHone(user.getPhone());
+        user.setIdRol("4");
         userPersistencePort.saveUser(user);
     }
 
     @Override
     public void saveOwner(User user) {
+        UserService.ValidDni(user.getDniNumber());
         UserService.ValidAge(user.getBirthDate());
         UserService.ValidPHone(user.getPhone());
+        user.setIdRol("2");
         userPersistencePort.saveUser(user);
     }
 }
