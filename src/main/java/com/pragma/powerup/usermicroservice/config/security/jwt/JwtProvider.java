71b073807220c07bdcd85d00064dc0e586195917
steven quintana <quintanasteven57@gmail.com>
2023-05-05 23:22:21 -0500

    add: jwt provider class✨

---
+++ src/main/java/com/pragma/powerup/usermicroservice/config/security/jwt/JwtProvider.java
@@ -0,0 +1,35 @@
+package com.pragma.powerup.usermicroservice.config.security.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+    private final Key secretKey;
+    @Value("${jwt.expiration}")
+    private int expiration;
+    public JwtProvider(@Value("${jwt.secret}") String key) {
+        this.secretKey = Keys.hmacShaKeyFor(key.getBytes());
+    }
+
+
+    public String generateToken(UserDetails userDetails) {
+        String rol = userDetails.getAuthorities().toString();
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .claim("roles", rol)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + expiration * 180L))
+                .signWith(secretKey,SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
