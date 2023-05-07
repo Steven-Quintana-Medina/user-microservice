de5771de8552e380850632de054cb433763fda74
steven quintana <quintanasteven57@gmail.com>
2023-05-07 16:50:15 -0500

    change: added the authorization filter✨

--- src/main/java/com/pragma/powerup/usermicroservice/config/security/MainSecurity.java
+++ src/main/java/com/pragma/powerup/usermicroservice/config/security/MainSecurity.java
@@ -1,12 +1,17 @@
 package com.pragma.powerup.usermicroservice.config.security;
 
+import com.pragma.powerup.usermicroservice.config.security.jwt.JwtEntryPoint;
+import com.pragma.powerup.usermicroservice.config.security.jwt.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,11 +21,24 @@ public class MainSecurity {
         return new BCryptPasswordEncoder();
     }
 
+    @Autowired
+    JwtEntryPoint jwtEntryPoint;
+
+    @Bean
+    public JwtTokenFilter jwtTokenFilter() {
+        return new JwtTokenFilter();
+    }
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeHttpRequests(requests -> requests.requestMatchers("/auth/login", "/create-user/**").permitAll()
-                //.requestMatchers("/user").hasRole("ADMIN")
-                .anyRequest().authenticated()).formLogin().disable();
+        http.cors().and().csrf().disable().authorizeHttpRequests(requests -> requests.requestMatchers("/auth/login", "/create-user/client/").permitAll()
+                        .requestMatchers("/create-user/owner/").hasRole("ADMIN")
+                        .anyRequest().authenticated()).formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
