bd2fd0fce4fb32f71f6c593e463187172f4e68f4
steven quintana <quintanasteven57@gmail.com>
2023-05-07 16:48:08 -0500

    change: added invalid credential exception handler✨

--- src/main/java/com/pragma/powerup/usermicroservice/config/ControllerAdvisor.java
+++ src/main/java/com/pragma/powerup/usermicroservice/config/ControllerAdvisor.java
@@ -4,6 +4,7 @@ import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.exceptions.
 import com.pragma.powerup.usermicroservice.domain.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -50,5 +51,11 @@ public class ControllerAdvisor {
             InvalidDniException invalidDniException) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(Collections.singletonMap(RESPONSE_ERROR_MESSAGE_KEY, INVALID_DNI));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<Map<String, String>> handleAuthenticationException(AuthenticationException noDataFoundException) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(Collections.singletonMap(RESPONSE_ERROR_MESSAGE_KEY, WRONG_CREDENTIALS_MESSAGE));
     }
 }
