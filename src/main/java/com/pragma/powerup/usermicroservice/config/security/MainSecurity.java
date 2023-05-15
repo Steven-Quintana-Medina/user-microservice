bef5cbb18c172247fe4aadd1c2f907d49370639d
steven quintana <quintanasteven57@gmail.com>
2023-05-14 23:51:44 -0500

    add: swagger implementation✨

--- src/main/java/com/pragma/powerup/usermicroservice/config/security/MainSecurity.java
+++ src/main/java/com/pragma/powerup/usermicroservice/config/security/MainSecurity.java
@@ -5,6 +5,8 @@ import com.pragma.powerup.usermicroservice.config.security.jwt.JwtTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,11 +31,15 @@ public class MainSecurity {
         return new JwtTokenFilter();
     }
 
-
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeHttpRequests(requests -> requests.requestMatchers("/auth/login", "/create-user/client/").permitAll()
-                        .requestMatchers("/create-user/owner/").hasRole("ADMIN")
+        http.cors().and().csrf().disable().authorizeHttpRequests(requests -> requests.requestMatchers("/auth/login", "/user/create-client/","/actuator/health","/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/user/create-owner/", "/user/owner/").hasRole("ADMIN")
                         .anyRequest().authenticated()).formLogin().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
