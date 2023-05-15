2ce5a717b3821600d7dda49cbdb84a220e3d4c3d
steven quintana <quintanasteven57@gmail.com>
2023-05-14 23:08:50 -0500

    fix: removing import from role class🚨

--- src/main/java/com/pragma/powerup/usermicroservice/domain/usecase/UserUseCase.java
+++ src/main/java/com/pragma/powerup/usermicroservice/domain/usecase/UserUseCase.java
@@ -3,7 +3,6 @@ package com.pragma.powerup.usermicroservice.domain.usecase;
 import com.pragma.powerup.usermicroservice.domain.api.IUserServicePort;
 import com.pragma.powerup.usermicroservice.domain.model.User;
 import com.pragma.powerup.usermicroservice.domain.services.UserService;
-import com.pragma.powerup.usermicroservice.domain.spi.IRolPersistencePort;
 import com.pragma.powerup.usermicroservice.domain.spi.IUserPersistencePort;
 
 public class UserUseCase implements IUserServicePort {
